ff29750c1d7226a47402a37b7b06ac0a716b6eac
root <root@localhost>
2016-03-21 17:00:14 +0800

    From C4droid (KiritoTRw)

---
+++ wartime_mapinfo.h
@@ -0,0 +1,16 @@
+/** WarTime-Project On GitHub
+*   https://github.com/Kiritow/WarTime-Project.git
+*/
+
+#ifndef WARTIME_MAPINFO
+#define WARTIME_MAPINFO
+
+struct _block
+{
+	
+};
+
+using MAP = _block;
+using MAPHANDLE = MAP*;
+
+#endif /* End of WARTIME_MAPINFO*/
