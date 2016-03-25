21eb0d66e234509ea7e111c8310cb1fe65aca0ef
Kirito <1362050620@qq.com>
2016-03-25 20:45:44 +0800

    Create wartime_support.h

---
+++ wartime_support.h
@@ -0,0 +1,15 @@
+/** WarTime-Project On GitHub
+*   https://github.com/Kiritow/WarTime-Project.git
+*/
+
+/** Support Functions
+*   Cross-Platform Header
+*/
+
+#ifndef WARTIME_SUPPORT
+#define WARTIME_SUPPORT
+
+/// Get MAC address of local device
+char* support_getmacaddress(char* TargetBuffer,int BufferSize);
+
+#endif /** End of WARTIME_SUPPORT */
