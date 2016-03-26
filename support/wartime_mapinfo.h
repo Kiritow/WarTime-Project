b62029a877f4ae0250fa515e3d49c0c8c2e26ac6
Kirito <1362050620@qq.com>
2016-03-26 17:17:29 +0800

    [In Progress] Add "struct point" and getDistance()
    Point Structure are added with an associated function getDistance().
    ATTENTION: This header is still in progress. Please don't include it until changing is finished.

--- support/wartime_mapinfo.h
+++ support/wartime_mapinfo.h
@@ -5,6 +5,8 @@
 #ifndef WARTIME_MAPINFO
 #define WARTIME_MAPINFO
 
+#include <cmath>
+
 struct _block
 {
 	
@@ -12,5 +14,16 @@ struct _block
 
 using MAP = _block;
 using MAPHANDLE = MAP*;
+
+struct _point
+{
+    int x,y;
+};
+using POINT = _point;
+using DURATION = double;
+DURATION getDistance(POINT a,POINT b)
+{
+    return std::sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y));
+}
 
 #endif /* End of WARTIME_MAPINFO*/
