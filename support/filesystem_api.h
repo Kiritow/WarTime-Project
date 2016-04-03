83115b94245aaab52a524e97c91fa4279d22f6d0
Kirito <1362050620@qq.com>
2016-04-03 20:23:09 +0800

    Create filesystem_api.h

---
+++ support/filesystem_api.h
@@ -0,0 +1,11 @@
+/** WarTime-Project On GitHub
+*   https://github.com/Kiritow/WarTime-Project.git
+*/
+
+#ifndef WARTIME_FILESYSTEM_API
+#define WARTIME_FILESYSTEM_API
+
+/// Delete a local file.
+int deletelocalfile(const char* FileName);
+
+#endif /// End of WARTIME_FILESYSTEM_API
