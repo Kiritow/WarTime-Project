28961aa4a04c08c173e4460be05175da33b8acbd
unknown <>
2016-06-02 09:05:42 +0800

    Description added.
    June 2nd, 2016

--- frame/blob.cpp
+++ frame/blob.cpp
@@ -1,7 +1,10 @@
+/** Blob : Binary Large Object.*/
+
 #ifdef __C4DROID__
 #include <cpplib/cpplib#busybox>
 #include <cpplib/cpplib#exception>
 #endif
+
 #include <string>
 #include <cstring>
 #include <cstdlib>
@@ -13,84 +16,87 @@ using namespace std;
 class blob
 {
 private:
-	struct _t { string str; };
-	struct _b
-	{
+    struct _t
+    {
+        string str;
+    };
+    struct _b
+    {
     public:
         /// Data Fields
-		void* data;
-		int size;
-		/// Default Constructor
-		_b()
-		{
-			data=nullptr;
-			size=0;
-		}
-		/// Destructor
-		~_b()
-		{
-			free(data);
-		}
-		/// Copy Operator and Copy Constructor
-		_b(const _b& inc)
-		{
-			size=inc.size;
-			data=malloc(size);
-			if(data==nullptr)
-			{
-				size=0;
-			}
-			else
-			{
-				memcpy(data,inc.data,size);
-			}
-		}
-		_b& operator = (const _b& inc)
-		{
-			if(this==&inc)
-			{
-				return *this;
-			}
-			free(data);
-			data=nullptr;
-			size=inc.size;
-			data=malloc(size);
-			if(data==nullptr)
-			{
-				size=0;
-			}
-			else
-			{
-				memcpy(data,inc.data,size);
-			}
-			return *this;
-		}
-		/// Move Operator and Move Constructor
-		_b& operator = (_b&& inc)
-		{
-			free(data);
-			size=inc.size;
-			data=inc.data;
-			inc.data=nullptr;
-			return *this;
-		}
-		_b(_b&& inc)
-		{
-			size=inc.size;
-			data=inc.data;
-			inc.data=nullptr;
-		}
-	};
-	public:
-	enum class blobtype { null,bin,text };
-	blobtype type;
-	_b bin;
-	_t text;
-	blob()
-	{
-		type=blobtype::null;
-	}
-	blob(blobtype inctype) : type(inctype) { }
+        void* data;
+        int size;
+        /// Default Constructor
+        _b()
+        {
+            data=nullptr;
+            size=0;
+        }
+        /// Destructor
+        ~_b()
+        {
+            free(data);
+        }
+        /// Copy Operator and Copy Constructor
+        _b(const _b& inc)
+        {
+            size=inc.size;
+            data=malloc(size);
+            if(data==nullptr)
+            {
+                size=0;
+            }
+            else
+            {
+                memcpy(data,inc.data,size);
+            }
+        }
+        _b& operator = (const _b& inc)
+        {
+            if(this==&inc)
+            {
+                return *this;
+            }
+            free(data);
+            data=nullptr;
+            size=inc.size;
+            data=malloc(size);
+            if(data==nullptr)
+            {
+                size=0;
+            }
+            else
+            {
+                memcpy(data,inc.data,size);
+            }
+            return *this;
+        }
+        /// Move Operator and Move Constructor
+        _b& operator = (_b&& inc)
+        {
+            free(data);
+            size=inc.size;
+            data=inc.data;
+            inc.data=nullptr;
+            return *this;
+        }
+        _b(_b&& inc)
+        {
+            size=inc.size;
+            data=inc.data;
+            inc.data=nullptr;
+        }
+    };
+public:
+    enum class blobtype { null,bin,text };
+    blobtype type;
+    _b bin;
+    _t text;
+    blob()
+    {
+        type=blobtype::null;
+    }
+    blob(blobtype inctype) : type(inctype) { }
 };
 
 /** Exception For Blob */
@@ -98,7 +104,7 @@ private:
 class curl_error : public basic_exception
 {
 public:
-    explicit curl_error(const string& inc):basic_exception(string("Error Occur while fetching page: ")+inc){}
+    explicit curl_error(const string& inc):basic_exception(string("Error Occur while fetching page: ")+inc) {}
 };
 
 /** Execute std::string shell command */
@@ -111,9 +117,9 @@ int strexec(const string& cmdstr)
 /** Get temp file */
 string gettmpfile()
 {
-	thread_local char buff[256];
-	gettmpfile(buff,256);
-	return string(buff);
+    thread_local char buff[256];
+    gettmpfile(buff,256);
+    return string(buff);
 }
 
 class atmp
@@ -137,45 +143,45 @@ private:
 
 blob getwebpage(const string& webpage) throw (curl_error)
 {
-	string cmd;
-	blob b;
-	b.type=blob::blobtype::text;
-	atmp file,efile,ecfile;
-	thread_local char buffer[1024];
-	bzero(buffer,1024);
-	cmd=(string)"curl "+webpage+" > "+file.name()+" 2> "+efile.name();
-	strexec(cmd);
-	cmd=(string)"grep 'curl' "+efile.name()+" > "+ecfile.name();
-	strexec(cmd);
-	if(getfilesize(ecfile.name().c_str())>3)
+    string cmd;
+    blob b;
+    b.type=blob::blobtype::text;
+    atmp file,efile,ecfile;
+    thread_local char buffer[1024];
+    bzero(buffer,1024);
+    cmd=(string)"curl "+webpage+" > "+file.name()+" 2> "+efile.name();
+    strexec(cmd);
+    cmd=(string)"grep 'curl' "+efile.name()+" > "+ecfile.name();
+    strexec(cmd);
+    if(getfilesize(ecfile.name().c_str())>3)
     {
         /// Error occur when curl...
         throw curl_error(webpage);
     }
-	FILE* fp=fopen(file.name().c_str(),"r");
-	b.text.str.clear();
-	while(!feof(fp))
-	{
-		fgets(buffer,1024,fp);
-		b.text.str.append(string(buffer));
-		bzero(buffer,1024);
-	}
-	fclose(fp);
-	return b;
+    FILE* fp=fopen(file.name().c_str(),"r");
+    b.text.str.clear();
+    while(!feof(fp))
+    {
+        fgets(buffer,1024,fp);
+        b.text.str.append(string(buffer));
+        bzero(buffer,1024);
+    }
+    fclose(fp);
+    return b;
 }
 
 blob loadbinfile(const string& binaryfile) throw(file_error,mem_error)
 {
-	int sz=getfilesize(binaryfile.c_str());
-	if(sz<0) throw file_error("File not exist");
-	void* dx=malloc(sz+8);
-	if(dx==nullptr) throw mem_error(sz+8);
-	memset(dx,0,sz+8);
-	blob b(blob::blobtype::bin);
-	FILE* fp=fopen(binaryfile.c_str(),"rb");
-	fread(dx,sz+8,1,fp);
-	fclose(fp);
-	b.bin.data=dx;
-	b.bin.size=sz+8;
-	return b;
+    int sz=getfilesize(binaryfile.c_str());
+    if(sz<0) throw file_error("File not exist");
+    void* dx=malloc(sz+8);
+    if(dx==nullptr) throw mem_error(sz+8);
+    memset(dx,0,sz+8);
+    blob b(blob::blobtype::bin);
+    FILE* fp=fopen(binaryfile.c_str(),"rb");
+    fread(dx,sz+8,1,fp);
+    fclose(fp);
+    b.bin.data=dx;
+    b.bin.size=sz+8;
+    return b;
 }
