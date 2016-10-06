06e267b265b5f0ba3a471eba0bf70e226aa91ea1
Kirito <1362050620@qq.com>
2016-10-06 22:09:30 +0800

    Create base64.h

---
+++ frame/base64.h
@@ -0,0 +1,21 @@
+/** CPPLIB Extends - Base64 And Base64 Raw Decoder.
+*   This File is created by HC TECH.
+*   Created On: 20161006
+*/
+
+#ifndef _cpplib_extends_base64
+#define _cpplib_extends_base64
+
+/** Base64 Basic Definitions
+*   Base64 Encode/Decode Functions
+*       unsigned char --> string (Encode) : string base64_encode(unsigned char*,int)
+*       string --> string (Decode) : string base64_decode(string)
+*/
+#include "base64_str.h"
+
+/** Base64 Raw Decoder
+*       string --> unsigned char : int base64_decode_raw(string,unsigned char*)
+*/
+#include "base64_raw.h"
+
+#endif ///  End of _cpplib_extends_base64
