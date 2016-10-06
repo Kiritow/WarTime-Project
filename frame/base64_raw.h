57c8dfb0d7d44e856947b694b14f5ae9649ba8e4
Kirito <1362050620@qq.com>
2016-10-06 22:09:13 +0800

    Create base64_raw.h

---
+++ frame/base64_raw.h
@@ -0,0 +1,59 @@
+/** CPPLIB Extends on Base64.h : Base64 - Decode to RawData
+*    Created By HC TECH.
+*    Created On: 20161006
+*/
+
+#ifndef _cpplib_extends_base64_raw
+#define _cpplib_extends_base64_raw
+
+#include "base64.h"
+#include <string>
+using namespace std;
+
+int base64_decode_raw(string const& encoded_string,unsigned char* outbuff)
+{
+    int in_len = encoded_string.size();
+    int i = 0;
+    int j = 0;
+    int in_ = 0;
+    unsigned char char_array_4[4], char_array_3[3];
+    int ret=0;
+
+    while (in_len-- && ( encoded_string[in_] != '=') && is_base64(encoded_string[in_]))
+    {
+        char_array_4[i++] = encoded_string[in_];
+        in_++;
+        if (i ==4)
+        {
+            for (i = 0; i <4; i++)
+                char_array_4[i] = base64_chars.find(char_array_4[i]);
+
+            char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+            char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+            char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+
+            for (i = 0; (i < 3); i++)
+                outbuff[ret++]= char_array_3[i];
+            i = 0;
+        }
+    }
+
+    if (i)
+    {
+        for (j = i; j <4; j++)
+            char_array_4[j] = 0;
+
+        for (j = 0; j <4; j++)
+            char_array_4[j] = base64_chars.find(char_array_4[j]);
+
+        char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+        char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+        char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+
+        for (j = 0; (j < i - 1); j++) outbuff[ret++] = char_array_3[j];
+    }
+
+    return ret;
+}
+
+#endif /// End of _cpplib_extends_base64_raw(base64_raw.h)
